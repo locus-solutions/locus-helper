28053c5dddaff138068204ded469cf1c31e1471e
synesta <synestamail2@gmail.com>
2022-02-16 05:13:17 -0500

    feat(fun): You needed this
    Fear Bo .
    
    Tm  Technologo   ..
    
    Shoutout  Bill Clinton
    
    Joe Bidn  Kurdistn  2040
    
    Shout out *********** Lexi  ****** Ka
    
    TThis  is  LKuca Ka a a
    Production ,
    My Mother is DEAD ) M<usic
    (
    
    REcord Label :"  Bob Tm
    Exponetnaial Interpotlation Implemn t
    
    Added, 2055
       Bob Technology,   Implemented FaceBook Security System,   Geniu
    
    Implemented await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const  await let const

---
+++ commands/fun/bobr.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "bobr",
+    category: "   Bo              ",
+    description: "be 🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫🦫",
+    aliases: [":beaver:", "bo", "🦫"],
+    usage: "bobr [@user | userID]",
+    run: async(client, message, args, util) => {
+        let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+
+        const embed = new MessageEmbed()
+        .setAuthor("Shout out My Nikka Beav")
+        .setDescription(`🦫🦫🦫   *🦫 * 🦫  🦫  🦫 ${member} 🦫 Bo You are This ***${util.randomizeNumber(1, 5000)}***🦫 Hi 🦫🦫🦫  🦫 🦫%  `)
+        .setFooter("☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️☎️")
+        .setColor("#926F5B");
+
+        await message.reply({
+            embeds: [embed]
+        });
+    }
+}
