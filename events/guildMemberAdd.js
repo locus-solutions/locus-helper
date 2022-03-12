17e1b7b599338ef463c1c159f0cdbc1b2c7e3b2c
synesta <synestamail2@gmail.com>
2022-03-12 18:33:31 -0500

    feat(logs): event logging for guild members when they join/leave
    I plan on adding a lot more related to guild events

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+const ms = require('ms');
+
+module.exports = async(client, member) => {
+    if(member.user.bot === true) {
+        return;
+    } else {
+        console.log(chalk.grey(`${member.user.username}#${member.user.discriminator} (ID: ${member.user.id}) joined ${member.guild.name}`))
+    }
+}
